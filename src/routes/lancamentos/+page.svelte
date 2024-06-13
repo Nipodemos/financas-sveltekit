be0740ad74dfa7fff8e449f1d624131047b575bc
Nipodemos <nipodemos13@gmail.com>
2024-06-12 23:35:09 -0300

    começo da tela de Lançamentos

---
+++ src/routes/lancamentos/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Button, Card, Header, Tooltip } from 'svelte-ux';
+</script>
+
+<Card title="Lançamentos">
+	<Header title="Title" subheading="Subheading" slot="header">
+		<div slot="actions">
+			<Tooltip title="Nova despesa">
+				<Button size="lg" icon="https://api.iconify.design/ph/plus-circle.svg" color="danger" />
+			</Tooltip>
+			<Tooltip title="Nova receita">
+				<Button size="lg" icon="https://api.iconify.design/ph/plus-circle.svg" color="success" />
+			</Tooltip>
+		</div>
+	</Header>
+</Card>
