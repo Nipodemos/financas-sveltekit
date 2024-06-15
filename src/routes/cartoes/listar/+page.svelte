46c40c79315715070e0b087dea39157ba72b12ab
Nipodemos <nipodemos13@gmail.com>
2024-06-15 00:52:48 -0300

    esboço incompleto da tela de listar cartoes

---
+++ src/routes/cartoes/listar/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Table } from 'svelte-ux';
+</script>
+
+<h2>Cartões de débito</h2>
+<Table
+	{data}
+	columns={[
+		{
+			name: 'name',
+			align: 'left'
+		},
+		{
+			name: 'calories',
+			align: 'right',
+			format: 'integer'
+		}
+	]}
+/>
+
+<h2>Cartões de crédito</h2>
