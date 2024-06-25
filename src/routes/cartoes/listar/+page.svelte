5ee753bd7d6d4a118f8f194357564ecf5c3f971a
Nipodemos <nipodemos13@gmail.com>
2024-06-24 23:20:20 -0300

    fui pra mexer na tela de cadastro de cartões mas não mexi nada

--- src/routes/cartoes/listar/+page.svelte
+++ src/routes/cartoes/listar/+page.svelte
@@ -3,19 +3,5 @@
 </script>
 
 <h2>Cartões de débito</h2>
-<Table
-	{data}
-	columns={[
-		{
-			name: 'name',
-			align: 'left'
-		},
-		{
-			name: 'calories',
-			align: 'right',
-			format: 'integer'
-		}
-	]}
-/>
 
 <h2>Cartões de crédito</h2>
