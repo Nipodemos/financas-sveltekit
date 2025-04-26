d67748af5259791a5e11e2935769934f3e07aed8
Nipodemos <nipodemos13@gmail.com>
2025-04-25 21:57:24 -0300

    sei lá, faz meses isso

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,66 +1,56 @@
+<script>
+	import '../app.css';
+	import { page } from '$app/stores';
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	import {
+		ChartPieSolid,
+		GridSolid,
+		MailBoxSolid,
+		UserSolid,
+		ArrowRightToBracketOutline,
+		EditOutline
+	} from 'flowbite-svelte-icons';
+	$: activeUrl = $page.url.pathname;
+</script>
 
-
-<Sidebar {activeurl}>
+<Sidebar {activeUrl}>
 	<SidebarWrapper>
 		<SidebarGroup>
-			<SidebarItem label="Dashboard">
+			<SidebarItem label="Dashboard" href="/">
 				<svelte:fragment slot="icon">
-					<ChartPieSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></ChartPieSolid>
+					<ChartPieSolid
+						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
 				</svelte:fragment>
 			</SidebarItem>
-			<SidebarItem label="Kanban" {spanclass}>
+			<SidebarItem label="Accordion" href="/docs/components/accordion">
 				<svelte:fragment slot="icon">
-					<GridSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></GridSolid>
-				</svelte:fragment>
-				<svelte:fragment slot="subtext">
-					<span class="inline-flex justify-center items-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
-						Pro
-					</span>
+					<GridSolid
+						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
 				</svelte:fragment>
 			</SidebarItem>
-			<SidebarItem label="Inbox" {spanclass}>
+			<SidebarItem label="Alert" href="/docs/components/alert">
 				<svelte:fragment slot="icon">
-					<MailBoxSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></MailBoxSolid>
-				</svelte:fragment>
-				<svelte:fragment slot="subtext">
-					<span class="inline-flex justify-center items-center p-3 ms-3 w-3 h-3 text-sm font-medium text-primary-600 bg-primary-200 rounded-full dark:bg-primary-900 dark:text-primary-200">
-						3
-					</span>
+					<MailBoxSolid
+						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
 				</svelte:fragment>
 			</SidebarItem>
-			<SidebarItem label="Sidebar" href="/docs/components/sidebar" active="{activeUrl" =="=" ' docs components sidebar'}>
+			<SidebarItem label="Sidebar" href="/docs/components/sidebar">
 				<svelte:fragment slot="icon">
-					<UserSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></UserSolid>
+					<UserSolid
+						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
 				</svelte:fragment>
 			</SidebarItem>
-			<SidebarItem label="Sign In">
+			<SidebarItem label="Badge" href="/docs/components/badge">
 				<svelte:fragment slot="icon">
-					<ArrowRightToBracketOutline class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></ArrowRightToBracketOutline>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Sign Up">
-				<svelte:fragment slot="icon">
-					<EditOutline class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></EditOutline>
+					<ArrowRightToBracketOutline
+						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
 				</svelte:fragment>
 			</SidebarItem>
 		</SidebarGroup>
 	</SidebarWrapper>
 </Sidebar>
-<slot></slot>
-<slot></slot><script>import "../app.css";
-import { page } from "$app/stores";
-import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from "flowbite-svelte";
-
-import {
-    ChartPieSolid,
-    GridSolid,
-    MailBoxSolid,
-    UserSolid,
-    ArrowRightToBracketOutline,
-    EditOutline,
-} from "flowbite-svelte-icons";
-
-let spanClass = "flex-1 ms-3 whitespace-nowrap";
-
-$:
-activeUrl = $page.url.pathname;</script><style></style>
